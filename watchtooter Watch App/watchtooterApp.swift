147806b8e2f215bf57026060efccf6143a22a99a
yamato3010 <yama3to10yama3@gmail.com>
2023-03-07 06:58:09 +0900

    Initial Commit

---
+++ watchtooter Watch App/watchtooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchtooterApp.swift
+//  watchtooter Watch App
+//
+//  Created by 大和 on 2023/03/07.
+//
+
+import SwiftUI
+
+@main
+struct watchtooter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watchtooter/watchtooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchtooterApp.swift
+//  watchtooter
+//
+//  Created by 大和 on 2023/03/07.
+//
+
+import SwiftUI
+
+@main
+struct watchtooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
